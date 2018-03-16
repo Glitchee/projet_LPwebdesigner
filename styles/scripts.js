bbc2a0cf8d6505fd99f9e03802da7795e2c6d9de
Unknown <cecile.pylypiw-staub@gmail.com>
2018-03-16 11:56:46 +0100

    Ajout du slider d'images

--- styles/scripts.js
+++ styles/scripts.js
@@ -1,3 +1,5 @@
+/*Lecteur vidéo*/
+
 document.addEventListener("DOMContentLoaded", function() { initialiseMediaPlayer(); }, false);
 
 var mediaPlayer;
@@ -88,3 +90,34 @@ document.getElementById('media-video').addEventListener('volumechange', function
     if (mediaPlayer.muted) changeButtonType(btn, 'unmute');
     else changeButtonType(btn, 'mute');
 }, false);*/
+
+/*Slider page à propos*/
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
